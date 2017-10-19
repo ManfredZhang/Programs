b20d29ff701df1fccd49ec3b46a69ede395124e9
161220169-Zhang Mingfang <zmf2468@163.com>
2017-10-19 06:11:33 +0000

    2-2 start

---
+++ Going/patest/native/tests/dummy.d
@@ -0,0 +1,2 @@
+/home/Manfred/ics2017/nexus-am/tests/cputest/build/native//tests/dummy.o: \
+ tests/dummy.c
---
+++ Going/patest/x86-nemu/tests/leap-year.d
@@ -0,0 +1,6 @@
+/home/Manfred/ics2017/nexus-am/tests/cputest/build/x86-nemu//tests/leap-year.o: \
+ tests/leap-year.c \
+ /home/Manfred/ics2017/nexus-am/tests/cputest/include/trap.h \
+ /home/Manfred/ics2017/nexus-am/am/am.h \
+ /home/Manfred/ics2017/nexus-am/am/arch/x86-nemu/include/arch.h \
+ /home/Manfred/ics2017/nexus-am/libs/klib/include/klib.h
