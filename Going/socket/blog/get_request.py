e0b78c73d98f53c2f99c73cbb777e2bd1242c654
Manfred Zhang <zmf2468@163.com>
2018-02-20 23:44:57 +0800

    pyy

--- Going/socket/blog/get_request.py
+++ Going/socket/blog/get_request.py
@@ -1,8 +1,36 @@
 import re
 from socket import *
 
+def parse_header(raw_data):
+    if not '\r\n\r\n' in raw_data:
+        print('Unable to parse the data:{}.'.format(raw_data))
+        return False
+    proto_headers, body = raw_data.split('\r\n\r\n', 1)
+    proto, headers = proto_headers.split('\r\n', 1)
+    ma = re.match(r'(GET|POST)\s+(\S+)\s+HTTP/1.1', proto)
+    if not ma:
+        print('unsupported protocol')
+        return False
+    method, path = ma.groups()
+    if path[0] == '/':
+        path = path[1:]
+    lis = path.split('?')
+    lis.append('')
+    rfile, query_string = lis[0:2]
+    params = [tuple((param+'=').split('=')[0:2])
+            for param in query_string.split('&')]
+    
+    ma_headers = re.findall(r'^\s*(.*?)\s*:\s*(.*?)\s*\r?$', headers, re.M)
+    headers = {item[0]:item[1] for item in ma_headers}
+    print("version\t: 1.1")
+    print("method\t: {}".format(method))
+    print("path\t: {}".format(rfile))
+    print("params\t: {}".format(params))
+    print("headers\t: {}".format(headers))
+
 def handle_request(request):
-    print(request)
+    #print(request)
+    parse_header(request)
     return 'Welcome to wiertons site'
 
 s = socket(AF_INET, SOCK_STREAM)
