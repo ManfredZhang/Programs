0ddff65c888e7d8b0352015bdb94d50fa5cf41a7
Manfred Zhang <zmf2468@163.com>
2018-02-25 16:41:41 +0800

    m

--- Going/socket/blog/httpServer.py
+++ Going/socket/blog/httpServer.py
@@ -48,7 +48,7 @@ sock.listen(100)
 
 #infinite loop
 while True:
-# maximum number of requests waiting
+    # maximum number of requests waiting
     conn, addr = sock.accept()
     request = conn.recv(1024)
     method = request.split(' ')[0]
@@ -57,40 +57,40 @@ while True:
     print 'Connect by: ', addr
     print 'Request is:\n', request
 
-#deal wiht GET method
-if method == 'GET':
-    if src == '/index.html':
-        content = index_content
-    elif src == '/T-mac.jpg':
-        content = pic_content
+    #deal wiht GET method
+    if method == 'GET':
+        if src == '/index.html':
+            content = index_content
+        elif src == '/T-mac.jpg':
+            content = pic_content
         elif src == '/reg.html':
-        content = reg_content
-    elif re.match('^/\?.*$', src):
-        entry = src.split('?')[1]      # main content of the request
+            content = reg_content
+        elif re.match('^/\?.*$', src):
+            entry = src.split('?')[1]      # main content of the request
+            content = 'HTTP/1.x 200 ok\r\nContent-Type: text/html\r\n\r\n'
+            content += entry
+            content += '<br /><font color="green" size="7">register successs!</p>'
+        else:
+            continue
+
+    
+    #deal with POST method
+    elif method == 'POST':
+        form = request.split('\r\n')
+        entry = form[-1]      # main content of the request
         content = 'HTTP/1.x 200 ok\r\nContent-Type: text/html\r\n\r\n'
         content += entry
         content += '<br /><font color="green" size="7">register successs!</p>'
+    
+    ######
+    # More operations, such as put the form into database
+    # ...
+    ######
+    
     else:
         continue
 
-
-#deal with POST method
-elif method == 'POST':
-    form = request.split('\r\n')
-    entry = form[-1]      # main content of the request
-    content = 'HTTP/1.x 200 ok\r\nContent-Type: text/html\r\n\r\n'
-    content += entry
-    content += '<br /><font color="green" size="7">register successs!</p>'
-
-######
-# More operations, such as put the form into database
-# ...
-######
-
-else:
-    continue
-
     conn.sendall(content)
-
-#close connection
-conn.close()
+    
+    #close connection
+    conn.close()
