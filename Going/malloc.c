deb7d84e1b9e23b7cf711bf113a87f0e8c42f165
Manfred <you@example.com>
2018-04-28 09:58:36 +0800

    malloc doing

---
+++ Going/malloc.c
@@ -0,0 +1,123 @@
+#include "malloc.h"
+
+#include <stdlib.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdint.h>
+
+
+//=========================================================
+// http://blog.codinglabs.org/articles/a-malloc-tutorial.html#1-
+//=========================================================
+
+#define BLOCK_SIZE 24
+
+extern void *sbrk(intptr_t increment);
+
+pthread_mutex_t mutex;
+void *first_block=NULL;
+
+typedef struct s_block *t_block;
+struct s_block {
+	size_t size;  /* 数据区大小 */
+	t_block next; /* 指向下个块的指针 */
+	int free;     /* 是否是空闲块 */
+	int padding;  /* 填充4字节，保证meta块长度为8的倍数 */
+	char data[1]  /* 这是一个虚拟字段，表示数据块的第一个字节，长度不应计入meta */
+};
+
+t_block find_block(t_block *last, size_t size) {
+	t_block b = first_block;
+	while(b && !(b->free && b->size >= size)) {
+		*last = b;
+		b = b->next;
+	}
+	return b;
+}
+
+t_block extend_heap(t_block last, size_t s) {
+	t_block b;
+	b = sbrk(0);
+	if(sbrk(BLOCK_SIZE + s) == (void *)-1)
+		return NULL;
+	b->size = s;
+	b->next = NULL;
+	if(last)
+		last->next = b;
+	b->free = 0;
+	return b;
+}
+
+void split_block(t_block b, size_t s) {
+	t_block new;
+	new = b->data + s;
+	new->size = b->size - s - BLOCK_SIZE ;
+	new->next = b->next;
+	new->free = 1;
+	b->size = s;
+	b->next = new;
+}
+
+size_t align8(size_t s) {
+	if(s & 0x7 == 0)
+		return s;
+	return ((s >> 3) + 1) << 3;
+}
+
+void *malloc_unsafe(size_t size) {
+	t_block b, last;
+	size_t s;
+	/* 对齐地址 */
+	s = align8(size);
+	if(first_block) {
+		/* 查找合适的block */
+		last = first_block;
+		b = find_block(&last, s);
+		if(b) {
+			/* 如果可以，则分裂 */
+			if ((b->size - s) >= ( BLOCK_SIZE + 8))
+				split_block(b, s);
+			b->free = 0;
+		} else {
+			/* 没有合适的block，开辟一个新的 */
+			b = extend_heap(last, s);
+			if(!b)
+				return NULL;
+		}
+	} else {
+		b = extend_heap(NULL, s);
+		if(!b)
+			return NULL;
+		first_block = b;
+	}
+	return b->data;
+}
+
+void free_unsafe(void *ptr) {
+
+}
+
+void* do_malloc(size_t size) {
+	pthread_mutex_lock(&mutex); // 上锁
+	void* result = malloc_unsafe(size); // 具体实现分配的算法
+	pthread_mutex_unlock(&mutex); // 解锁
+	return result;
+}
+
+void do_free(void *ptr) {
+    pthread_mutex_lock(&mutex); // 上锁
+	free_unsafe(ptr); // 具体实现释放的算法
+	pthread_mutex_unlock(&mutex); // 解锁
+}
+
+
+/*
+void* do_malloc(size_t size) {
+	return malloc(size);
+}
+
+void do_free(void *ptr) {
+	free(ptr);
+}
+*/
